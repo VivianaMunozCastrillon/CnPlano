b5c2bfa2ab9e0362292bae62f82785fa8103bc29
VivianaMuñozCastrillon <vivi84castri@gmail.com>
2023-02-28 13:59:54 -0500

    Agregar archivos de proyecto.

---
+++ CnPlano/ClsPlano.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnPlano
+{
+    internal class ClsPlano
+    {
+        //Declaracion de variables
+        private double x;
+        private double y;
+
+
+
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        //Metodos
+        public void CargarValores()
+        {
+            Console.Write("Ingrese el valor de X : ");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor de Y");
+            y = double.Parse(Console.ReadLine());
+
+
+
+
+        }
+
+
+        public void Cuadrante()
+        {
+            if (x > 0 & y > 0)
+            {
+                Console.WriteLine("El punto esta en el primer cuadrante");
+            }
+            else if (x < 0 & y > 0)
+            {
+                Console.WriteLine("El punto esta en el segundo cuadrante");
+            }
+
+            else if (x < 0 & y < 0)
+            {
+                Console.WriteLine("El punto esta en el tercer cuadrante");
+            }
+            else if (x > 0 & y < 0)
+            {
+                Console.WriteLine("El punto esta en el cuarto cuadrante");
+
+            }
+            else
+            {
+                Console.WriteLine("El punto esta en el origen");
+            }
+        }
+    }
+}
---
+++ CnPlano/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CnPlano
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Nombre:Viviana Muñoz
+        /// Fecha:26/02/2023
+        /// Descripcion:Programa que solicita un valor de un punto X y Y e imrime en que cuadrante se encuentra el punto.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            //Instancia de clase
+            ClsPlano plano = new ClsPlano();
+
+
+            //Llamada de metodos
+            plano.CargarValores();
+            plano.Cuadrante();
+
+
+            Console.ReadKey();
+        }
+    }
+}
